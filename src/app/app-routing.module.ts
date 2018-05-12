8ad48ca227fd8a8513a231471940a6504df8dcf2
vanvibig <vanvibig@gmail.com>
2018-05-12 17:10:41 +0700

    Angular 6 Tutorial - 23 - Routing and Navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { DepartmentListComponent } from './department-list/department-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'department', component: DepartmentListComponent}
+  {path: 'employee', component: EmployeeListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const RoutingComponent = [DepartmentListComponent, EmployeeListComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, RoutingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoutingComponent
   ],
   imports: [
     BrowserModule,
