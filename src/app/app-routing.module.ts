079fdb7568f669d0aa0ede58330a8182f795d1c1
vanvibig <vanvibig@gmail.com>
2018-05-13 15:38:25 +0700

    Angular 6 Tutorial - 25 - Route Parameters

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { DepartmentDetailComponent } from './department-detail/department-detail.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { DepartmentListComponent } from './department-list/department-list.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   // {path: '', component: DepartmentListComponent},
   {path: '', redirectTo: '/department', pathMatch: 'full'},
   {path: 'department', component: DepartmentListComponent},
+  {path: 'department/:id', component: DepartmentDetailComponent},
   {path: 'employee', component: EmployeeListComponent},
   {path: "**", component: PageNotFoundComponent}
 ];
@@ -20,5 +22,6 @@ export class AppRoutingModule { }
 export const RoutingComponent = [
   DepartmentListComponent,
   EmployeeListComponent,
-  PageNotFoundComponent
+  PageNotFoundComponent,
+  DepartmentDetailComponent
 ];
--- src/app/department-list/department-list.component.ts
+++ src/app/department-list/department-list.component.ts
@@ -1,19 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-department-list',
+  selector: "app-department-list",
   template: `
-    <p>
-      department-list works!
-    </p>
+    <h3>Department List</h3>
+    <ul class="items">
+      <li class="btn btn-default" (click)="onSelect(department)" *ngFor="let department of departments">
+          <span class="badge">{{department.id}}</span> {{department.name}}
+      </li>
+    </ul>
   `,
   styles: []
 })
 export class DepartmentListComponent implements OnInit {
+  departments = [
+    { id: 1, name: "Angular" },
+    { id: 2, name: "Node" },
+    { id: 3, name: "MongoDB" },
+    { id: 4, name: "Ruby" },
+    { id: 5, name: "Bootstrap" }
+  ];
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSelect(department){
+    this.router.navigate(['/department' ,department.id]);
   }
-
 }
